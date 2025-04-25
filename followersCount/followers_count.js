6ae87d2fe34d01414dd8041a43952bb7d90f6c8b
V3lvvy <4rwanubar@gmail.com>
2025-04-24 23:48:26 -0400

    Added followers count reset feature

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -14,5 +14,13 @@ function increaseCount() {
         alert("Your instagram post gained 10 followers, Congratulations!")
     } else if (count === 20) {
         alert("Your Instagram post gained 20 followers, Keep it up!")
+    } 
+ }
+
+ function resetCount() {
+    count = 0;
+    displayCount();
+    if (count === 0) {
+        alert("Your followers count has been reset.")
     }
  }
