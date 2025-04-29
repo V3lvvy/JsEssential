caca6e085e1300d6bbc3c851376f442cf558030f
V3lvvy <4rwanubar@gmail.com>
2025-04-29 10:14:28 -0400

    Added total length feature

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -30,7 +30,7 @@ function endTest() {
 
     // Display the results
     var outputDiv = document.getElementById("output");
-    outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" + "<p>Words Typed: " + typedWords + "</p>" + "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" + "<p>Words Per Minute (WPM): " + wpm + "</p>";
+    outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" + "<p>Total Length: " + userTypedText.length + "</p>" + "<p>Words Typed: " + typedWords + "</p>" + "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" + "<p>Words Per Minute (WPM): " + wpm + "</p>";
 
     // Reset the button
     var button = document.getElementById("btn");
