5c416f3c2ea49113fd6ccbfe5ce29eab19dd8742
V3lvvy <4rwanubar@gmail.com>
2025-05-01 09:28:54 -0400

    Created the debugger code

---
+++ DebugCode/debug_code.js
@@ -0,0 +1,27 @@
+function performOperation() {
+    // Get user input from input fields
+    let num1 = parseInt(document.getElementById("input1").value);
+    let num2 = parseInt(document.getElementById("input2").value);
+    // check if inputs are valid numbers
+    if (!isNaN(num1) && !isNaN(num2)) {
+    // perform the operation
+        let result = multiply(num1, num2);
+        // display result
+        displayResult(result);
+    } else {
+        displayResult("Please enter valid numbers");
+    }
+}
+
+function multiply(a, b) {
+    // introduce a debugger statement to pause execuation
+    debugger;
+    // multiply the numbers
+    return a * b;
+}
+
+function displayResult(result) {
+    // display the result in the paragrapth element
+    const resultElement = document.getElementById("result");
+    resultElement.textContent = `The result is: ${result}`;
+}
