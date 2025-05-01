d48011ad2f2bf6b31619085072faa524ba7349cc
V3lvvy <4rwanubar@gmail.com>
2025-05-01 11:47:11 -0400

    Added other arithmetic operations in the debugger

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -1,14 +1,22 @@
-function performOperation() {
+function performOperation(op) {
     // Get user input from input fields
     let num1 = parseInt(document.getElementById("input1").value);
     let num2 = parseInt(document.getElementById("input2").value);
     // check if inputs are valid numbers
     if (!isNaN(num1) && !isNaN(num2)) {
     // perform the operation
-        let result = multiply(num1, num2);
+        let result = "" 
+        if (op == "m") {
+            result = multiply(num1, num2);
+        } else if (op == "a") {
+            result = add(num1, num2);
+        } else if (op == "d") {
+            result = divide(num1, num2);
+        }
         // display result
         displayResult(result);
     } else {
+        debugger;
         displayResult("Please enter valid numbers");
     }
 }
@@ -18,6 +26,16 @@ function multiply(a, b) {
     debugger;
     // multiply the numbers
     return a * b;
+}
+
+function add(a, b) {
+    debugger;
+    return a + b;
+
+}
+function divide(a, b) {
+    debugger;
+    return a / b;
 }
 
 function displayResult(result) {
